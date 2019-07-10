6fead25f00ad6ca217c6c86a78ea6e359b075869
Your Name <you@example.com>
2019-07-11 00:28:45 +0100

    added generated contacts and get methods

---
+++ src/controllers/index.ts
@@ -0,0 +1,25 @@
+"use strict";
+const contacts = require("../../data/contacts.json");
+
+function getContacts() {
+  const unblockContact = contacts.filter(
+    (contact: any) => contact.isBlocked === false
+  );
+  return unblockContact;
+}
+function getBlockedContacts() {
+  const blockContact = contacts.filter(
+    (contact: any) => contact.isBlocked === true
+  );
+  return blockContact;
+}
+function getContact(contactID: number) {
+  const contact = contacts.filter((contact: any) => contact.id === contactID);
+  if (!contact.length) {
+    throw new Error("Contact not found");
+  }
+  return contact[0];
+}
+exports.getContacts = getContacts;
+exports.getBlockedContacts = getBlockedContacts;
+exports.getContact = getContact;
