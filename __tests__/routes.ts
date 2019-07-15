2bbbc06333c321ca1e6416ff9e9b83d2e239b787
Your Name <you@example.com>
2019-07-15 19:13:38 +0100

    testing to get single contact

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -55,7 +55,7 @@ describe('GET /api/contacts', () => {
     });
 });
 
-describe('api/contacts/blocked', () => {
+describe('GET /api/contacts/blocked', () => {
     test('responds with all the contacts that are blocked', () => {
         return request(app)
             .get('/api/contacts/blocked')
@@ -93,6 +93,39 @@ describe('api/contacts/blocked', () => {
                     company_name: expect.any(String),
                     isBlocked: true,
                     created: expect.any(String),
+                });
+            });
+    });
+});
+describe('GET /api/contact/:contactId', () => {
+    test('responds with the right contact', () => {
+        return request(app)
+            .get('/api/contact/45745c60-7111-11e8-9c9c-2d42b21b1a3e')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.data).toEqual({
+                    id: '45745c60-7111-11e8-9c9c-2d42b21b1a3e',
+                    first_name: 'Elaine',
+                    last_name: 'Baine',
+                    phone: '188-617-4792',
+                    email: 'ebaine0@skype.com',
+                    str_address: '09711 Badeau Place',
+                    gender: 'Female',
+                    company_name: 'Skibox',
+                    isBlocked: false,
+                    created: '2019-04-18T08:20:48+01:00',
+                });
+            });
+    });
+    test('that it return Not Found message when id is not found', () => {
+        return request(app)
+            .get('/api/contact/662827-8a')
+            .set('Accept', 'application/json')
+            .expect(404)
+            .then(response => {
+                expect(response.body).toStrictEqual({
+                    error: 'Contact not found',
                 });
             });
     });
