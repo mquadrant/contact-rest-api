b74855e59e7bb4c1d29975d1a219d763dafc8bd2
Your Name <you@example.com>
2019-07-15 18:44:59 +0100

    testing api/contacts/blocked routes

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -11,6 +11,8 @@ describe('GET /api', () => {
                 expect(response.body).toStrictEqual({ message: 'All is well' });
             });
     });
+});
+describe('GET /api/contacts', () => {
     test('responds with json with all contacts', () => {
         return request(app)
             .get('/api/contacts')
@@ -27,6 +29,69 @@ describe('GET /api', () => {
                     gender: expect.any(String),
                     company_name: expect.any(String),
                     isBlocked: expect.any(Boolean),
+                    created: expect.any(String),
+                });
+            });
+    });
+    test('responds with all contacts having isBlock field to be false', () => {
+        return request(app)
+            .get('/api/contacts')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.data).toContainEqual({
+                    id: expect.any(String),
+                    first_name: expect.any(String),
+                    last_name: expect.any(String),
+                    phone: expect.any(String),
+                    email: expect.any(String),
+                    str_address: expect.any(String),
+                    gender: expect.any(String),
+                    company_name: expect.any(String),
+                    isBlocked: false,
+                    created: expect.any(String),
+                });
+            });
+    });
+});
+
+describe('api/contacts/blocked', () => {
+    test('responds with all the contacts that are blocked', () => {
+        return request(app)
+            .get('/api/contacts/blocked')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.data).toContainEqual({
+                    id: expect.any(String),
+                    first_name: expect.any(String),
+                    last_name: expect.any(String),
+                    phone: expect.any(String),
+                    email: expect.any(String),
+                    str_address: expect.any(String),
+                    gender: expect.any(String),
+                    company_name: expect.any(String),
+                    isBlocked: expect.any(Boolean),
+                    created: expect.any(String),
+                });
+            });
+    });
+    test('that the isBlocked field is always true', () => {
+        return request(app)
+            .get('/api/contacts/blocked')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.data).toContainEqual({
+                    id: expect.any(String),
+                    first_name: expect.any(String),
+                    last_name: expect.any(String),
+                    phone: expect.any(String),
+                    email: expect.any(String),
+                    str_address: expect.any(String),
+                    gender: expect.any(String),
+                    company_name: expect.any(String),
+                    isBlocked: true,
                     created: expect.any(String),
                 });
             });
