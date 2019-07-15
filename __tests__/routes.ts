d31b68bb70eaeb85058212d6d32a0e0858b0801e
Your Name <you@example.com>
2019-07-15 18:19:43 +0100

    testing  /api route

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -1,3 +1,14 @@
-describe('testing', () => {
-    it('should test', () => {});
+import request from 'supertest';
+import app from '../src/app';
+
+describe('GET /api', () => {
+    test('responds with json', () => {
+        return request(app)
+            .get('/api')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body).toBe({ message: 'All is well' });
+            });
+    });
 });
