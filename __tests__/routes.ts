499c2f44293317b54b32d46912313e65aecb3394
Your Name <you@example.com>
2019-07-15 18:27:11 +0100

    testing /api/contacts route

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -8,7 +8,27 @@ describe('GET /api', () => {
             .set('Accept', 'application/json')
             .expect(200)
             .then(response => {
-                expect(response.body).toBe({ message: 'All is well' });
+                expect(response.body).toStrictEqual({ message: 'All is well' });
+            });
+    });
+    test('responds with json with all contacts', () => {
+        return request(app)
+            .get('/api/contacts')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.data).toContainEqual({
+                    id: expect.any(String),
+                    first_name: expect.any(String),
+                    last_name: expect.any(String),
+                    phone: expect.any(String),
+                    email: expect.any(String),
+                    str_address: expect.any(String),
+                    gender: expect.any(String),
+                    company_name: expect.any(String),
+                    isBlocked: expect.any(Boolean),
+                    created: expect.any(String),
+                });
             });
     });
 });
