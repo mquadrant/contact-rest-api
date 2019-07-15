f52796c61df0fea757a281f9c8e0c83fd6f99bae
Your Name <you@example.com>
2019-07-15 14:12:21 +0100

    test should return all the blocked contacts

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,4 +1,4 @@
-import { getContacts } from '../src/controllers/index';
+import { getContacts, getBlockedContacts } from '../src/controllers/index';
 
 describe('API Controller methods', () => {
     test('getContacts should return all the contacts', () => {
@@ -17,5 +17,23 @@ describe('API Controller methods', () => {
         });
         //check that the right date format is passed
         expect(isNaN(new Date(contacts[0].created).getDate())).toBeFalsy;
+    });
+
+    test('getBlockedContacts should return all the blocked contacts', () => {
+        const blockedContacts = getBlockedContacts();
+        expect(blockedContacts).toContainEqual({
+            id: expect.any(String),
+            first_name: expect.any(String),
+            last_name: expect.any(String),
+            phone: expect.any(String),
+            email: expect.any(String),
+            str_address: expect.any(String),
+            gender: expect.any(String),
+            company_name: expect.any(String),
+            isBlocked: expect.any(Boolean),
+            created: expect.any(String),
+        });
+        //check that the right date format is passed
+        expect(isNaN(new Date(blockedContacts[0].created).getDate())).toBeFalsy;
     });
 });
