7b0840d453538f28a719032fe58326132478a7b1
Benjamin Mark <mquadrant@gmail.com>
2019-07-25 08:57:19 +0100

    contact model with a schema

---
+++ src/models/contactModel.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+
+// CONTACT SCHEMA
+const contactSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: [true, "A contact must have first name"],
+        trim: true,
+        maxlength: [
+            40,
+            "A contact name must have less or equal to 40 characters",
+        ],
+    },
+    last_name: {
+        type: String,
+        trim: true,
+        maxlength: [
+            40,
+            "A contact name must have less or equal to 40 characters",
+        ],
+    },
+    phone: {
+        type: String,
+        required: [true, "A contact must have a phone number"],
+        trim: true,
+        maxlength: [14, "A contact number should not be more than 11 digit"],
+    },
+    email: {
+        type: String,
+        trim: true,
+    },
+    str_address: {
+        type: String,
+        trim: true,
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: ["Female", "Male"],
+            message: "Gender is either: female, male",
+        },
+    },
+    company_name: {
+        type: String,
+        trim: true,
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+export default Contact;
