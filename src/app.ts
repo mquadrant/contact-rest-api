ddb5c1536c74babc38d55c96b76f49119e273c3e
Benjamin Mark <mquadrant@gmail.com>
2019-07-25 08:56:50 +0100

    changed to correct path for the dist folder

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import contactsRouter from "./routes/contacts";
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "../../views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/api", contactsRouter);
+app.use("/api/v1", contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(_req, _res, next) {
