d4c9fa098361c2e0d217d9ddb367dbd0d5ecd243
Benjamin Mark <mquadrant@gmail.com>
2019-07-26 14:48:39 +0100

    specified production and development error

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,5 +1,33 @@
 import express from "express";
 
+const sendErrorDev = (err: any, res: express.Response) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err: any, res: express.Response) => {
+    //operational error that we trust and send message to the client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+        //programming or other unknown error: dont leak error detail
+    } else {
+        // log the error
+        console.error("ERROR", err);
+        //send the generic error message
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong!",
+        });
+    }
+};
+
 export = (
     err: any,
     _req: express.Request,
@@ -9,9 +37,9 @@ export = (
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
     // render the error page
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-    // res.render("error");
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(err, res);
+    }
 };
