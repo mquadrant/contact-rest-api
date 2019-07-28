128a9de90e771909a14d59f135f6184aadc4c735
Benjamin Mark <mquadrant@gmail.com>
2019-07-28 23:30:27 +0100

    change header to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <Header></Header>
         </div>
     );
 }
