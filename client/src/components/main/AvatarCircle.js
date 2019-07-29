f3164268c2c015669cda6b2d7c30f7ce388f024d
Benjamin Mark <mquadrant@gmail.com>
2019-07-29 15:31:53 +0100

    modified for shadow

--- client/src/components/main/AvatarCircle.js
+++ client/src/components/main/AvatarCircle.js
@@ -4,9 +4,9 @@ import GeneralImage from "./../../images/final.png";
 
 const Avatar = styled.div`
     width: 50px;
+    height: 50px;
     border-top: 5px;
     border-radius: 48%;
-    height: 50px;
     background: #000 url(${GeneralImage}) center no-repeat;
     background-size: contain;
     ${props =>
@@ -17,6 +17,18 @@ const Avatar = styled.div`
             bottom: ${props => props.bottom};
             right: ${props => props.right};
             left: ${props => props.left};
+        `};
+    ${props =>
+        props.size &&
+        css`
+            width: ${props => `${props.size}px`};
+            height: ${props => `${props.size}px`};
+        `};
+    ${props =>
+        props.shadow &&
+        css`
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
+                0 1px 2px rgba(0, 0, 0, 0.24);
         `};
 `;
 export default function AvatarCircle(props) {
