bf333d0906229e899a90a32c138ccba8e4270907
Benjamin Mark <mquadrant@gmail.com>
2019-07-30 00:45:01 +0100

    added an optional border

--- client/src/components/main/AvatarCircle.js
+++ client/src/components/main/AvatarCircle.js
@@ -36,6 +36,11 @@ const Avatar = styled.div`
             box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
                 0 1px 2px rgba(0, 0, 0, 0.24);
         `};
+    ${props =>
+        props.border &&
+        css`
+            border: 1px solid #f0f0f0;
+        `};
 `;
 export default function AvatarCircle(props) {
     return <Avatar {...props}></Avatar>;
