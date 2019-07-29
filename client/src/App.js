c4837a08b662a05ecfd113fd2b47fea51041bb0f
Benjamin Mark <mquadrant@gmail.com>
2019-07-29 21:48:10 +0100

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Main from "./components/main/main";
+import Add from "./components/Add/";
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
-            <Main></Main>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route exact path="/add-contact" component={Add} />
+                </Switch>
+            </Router>
         </div>
     );
 }
