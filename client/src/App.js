3781aabfdf60f57b0d96ea78e7b321f97247e0ce
Benjamin Mark <mquadrant@gmail.com>
2019-07-29 13:53:59 +0100

    added main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
+import Main from "./components/main/main";
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <Main></Main>
         </div>
     );
 }
