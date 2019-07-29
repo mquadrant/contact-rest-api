341e6e6204a02858e6cb05f86d8626386335f022
Benjamin Mark <mquadrant@gmail.com>
2019-07-29 23:05:21 +0100

    moved the route to the Main

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,13 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Main from "./components/main/main";
-import Add from "./components/Add/";
 
 function App() {
     return (
         <div className="App">
-            <Header></Header>
             <Router>
-                <Switch>
-                    <Route exact path="/" component={Main} />
-                    <Route exact path="/add-contact" component={Add} />
-                </Switch>
+                <Header></Header>
+                <Main></Main>
             </Router>
         </div>
     );
