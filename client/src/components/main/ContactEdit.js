e4abaecb81f452bd3b86def3deb1078ca3f89c82
Benjamin Mark <mquadrant@gmail.com>
2019-07-30 15:31:58 +0100

    addes states to form

--- client/src/components/main/ContactEdit.js
+++ client/src/components/main/ContactEdit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const ContactView = styled.div`
@@ -26,6 +26,7 @@ const ContactView = styled.div`
         border: none;
         border-radius: 4px;
         cursor: pointer;
+        outline: none;
     }
     input[type="reset"] {
         width: 100%;
@@ -35,6 +36,7 @@ const ContactView = styled.div`
         border: none;
         border-radius: 4px;
         cursor: pointer;
+        outline: none;
     }
 
     input[type="submit"]:hover {
@@ -49,39 +51,64 @@ const FlexContainer = styled.div`
     display: flex;
 `;
 export default function ContactEdit() {
+    let defaultContact = {
+        fname: "John",
+        lname: "Benedict",
+        email: "John@gmail.com",
+        phone: "08965377568",
+        address: "Ocean dream avenue",
+        gender: "Male",
+        company: "Shell company",
+    };
+    const [values, setValues] = useState(defaultContact);
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    };
+    const handleSubmit = event => {
+        event.preventDefault();
+    };
+    const handleReset = () => {
+        setValues(defaultContact);
+    };
     return (
         <ContactView>
-            <form>
+            <form onSubmit={handleSubmit} onReset={handleReset}>
                 <FlexContainer>
                     <div style={{ margin: "10px", width: "50%" }}>
                         <label for="fname">First Name</label>
                         <input
                             type="text"
                             id="fname"
-                            name="firstname"
-                            value="John"
+                            name="fname"
+                            onChange={handleChange}
+                            value={values.fname}
                         />
 
                         <label for="phone">Phone</label>
                         <input
                             type="text"
-                            id="lname"
-                            name="lastname"
+                            id="phone"
+                            name="phone"
                             value="08965377568"
+                            onChange={handleChange}
+                            value={values.phone}
                         />
                         <label for="street">Street Address</label>
                         <input
                             type="text"
-                            id="lname"
-                            name="lastname"
-                            value="Ocean dream avenue"
+                            id="address"
+                            name="address"
+                            onChange={handleChange}
+                            value={values.address}
                         />
                         <label for="company">Company</label>
                         <input
                             type="text"
-                            id="lname"
-                            name="lastname"
-                            value="Shell company"
+                            id="company"
+                            name="company"
+                            onChange={handleChange}
+                            value={values.company}
                         />
                     </div>
                     <div style={{ margin: "10px", width: "50%" }}>
@@ -89,21 +116,26 @@ export default function ContactEdit() {
                         <input
                             type="text"
                             id="lname"
-                            name="lastname"
-                            value="Benedict"
+                            name="lname"
+                            onChange={handleChange}
+                            value={values.lname}
                         />
                         <label for="email">Email</label>
                         <input
                             type="email"
-                            id="lname"
-                            name="lastname"
-                            value="John@gmail.com"
+                            id="email"
+                            name="email"
+                            onChange={handleChange}
+                            value={values.email}
                         />
                         <label for="gender">Gender</label>
-                        <select id="gender" name="gender">
-                            <option value="Male" select>
-                                Male
-                            </option>
+                        <select
+                            id="gender"
+                            name="gender"
+                            onChange={handleChange}
+                            value={values.gender}
+                        >
+                            <option value="Male">Male</option>
                             <option value="Female">Female</option>
                         </select>
                         <input type="reset" value="Reset" />
