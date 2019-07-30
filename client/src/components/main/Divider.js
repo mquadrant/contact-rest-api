733bc6fa0ee4e524c90413c1e2f4385dbe763c43
Benjamin Mark <mquadrant@gmail.com>
2019-07-30 12:23:01 +0100

    made it fully styled component

--- client/src/components/main/Divider.js
+++ client/src/components/main/Divider.js
@@ -1,5 +1,7 @@
-import React from "react";
+import styled, { css } from "styled-components";
 
-export default function Divider() {
-    return <hr style={{ border: "solid 0.5px #f0f0f0" }} />;
-}
+export default styled.hr`
+    border: solid 0.5px #f0f0f0;
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+`;
