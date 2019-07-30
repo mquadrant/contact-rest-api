af51d0abee3718b01b005bd53b1bd5c8a80b8998
Benjamin Mark <mquadrant@gmail.com>
2019-07-30 01:47:39 +0100

    removed the tray

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
-import Search from "./Search";
-import ContactList from "./ContactList";
-import Divider from "./Divider";
-import AvatarCircle from "./AvatarCircle";
-import ContactEdit from "./ContactEdit";
+import {
+    Route,
+    Switch,
+    Link,
+    NavLink,
+    withRouter,
+    BrowserRouter as Router,
+} from "react-router-dom";
+import Home from "../Pages/Home";
+import Add from "../Pages/Add";
+import SideTray from "./SideTray";
+import Block from "../Pages/Block";
 
 const MainBody = styled.div`
     width: 100%;
@@ -15,128 +22,17 @@ const MainBody = styled.div`
     position: relative;
 `;
 
-const SideTray = styled.div`
-    position: absolute;
-    width: 90px;
-    height: auto;
-    top: 41%;
-    left: -90px;
-    z-index: 1000;
-    font-size: 12px;
-    background: white;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all 0.5s;
-    :hover {
-        left: 0;
-    }
-    .trayIcon {
-        padding: 15px 5px;
-        border-bottom: 0.5px solid #f0f0f0;
-        transition: all 0.3s;
-    }
-    .trayIcon:hover {
-        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-        background-color: #f0f0f0;
-        cursor: pointer;
-    }
-    .trayIcon i {
-        margin-right: 10px;
-    }
-`;
-
-const Beacon = styled.div`
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    top: 0;
-    right: -44%;
-    color: #fff;
-    font-size: 16px;
-    font-weight: 600;
-    text-align: center;
-    line-height: 35px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    background: #039fc7;
-    :hover {
-        cursor: pointer;
-    }
-`;
-
-const Container = styled.div`
-    display: flex;
-
-    background: #fff;
-    border-radius: 2px;
-    margin: 1rem;
-    position: relative;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-
-    height: 450px;
-    width: 60%;
-    margin-top: 30px;
-    @media (max-width: 768px) {
-        width: 700px;
-    }
-`;
-
-const Master = styled.div`
-    background: #fff;
-    width: 35%;
-    padding: 5px 0;
-    border-right: 1px solid #f0f0f0;
-`;
-
-const Detail = styled.div`
-    width: 100%;
-`;
-export default function main() {
+function Main(props) {
     return (
         <MainBody>
-            <Container>
-                <Master>
-                    <Search></Search>
-                    <div>
-                        <ContactList></ContactList>
-                        <Divider></Divider>
-                    </div>
-                </Master>
-                <Detail>
-                    <div
-                        style={{
-                            background: "#039fc7",
-                            width: "100%",
-                            height: "100px",
-                            position: "relative",
-                        }}
-                    >
-                        <AvatarCircle
-                            size="70"
-                            position="absolute"
-                            top="50%"
-                            right="50%"
-                        ></AvatarCircle>
-                    </div>
-                    <ContactEdit></ContactEdit>
-                </Detail>
-            </Container>
-            <SideTray>
-                <div className="trayIcon">
-                    <i class="fas fa-home" />
-                    Home
-                </div>
-                <div className="trayIcon">
-                    <i class="fas fa-user-plus" />
-                    Add
-                </div>
-                <div className="trayIcon">
-                    <i className="fas fa-user-lock" />
-                    Blocked
-                </div>
-                <Beacon>
-                    <i class="fas fa-star"></i>
-                </Beacon>
-            </SideTray>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/add-contact" component={Add} />
+                <Route exact path="/blocked" component={Block} />
+            </Switch>
+            <SideTray {...props}></SideTray>
         </MainBody>
     );
 }
+
+export default withRouter(Main);
