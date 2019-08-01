2239ed10d80720ef2ca8754a2d0c0a973d406880
Benjamin Mark <mquadrant@gmail.com>
2019-08-01 11:33:28 +0100

    redesign the width of the nav

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -6,10 +6,15 @@ const header = {
     height: "52px",
     display: "flex",
     flexDirection: "row",
-    justifyContent: "right",
+    justifyContent: "center",
     alignItems: "center",
     color: "white",
     padding: "0 20px",
+    position: "fixed",
+    width: "57%",
+    right: "50%",
+    left: "50%",
+    transform: "translate(-50%, 0)",
 };
 
 const MenuIcons = styled.span`
@@ -21,6 +26,7 @@ const MenuIcons = styled.span`
 const Logo = styled.div`
     margin-left: 10px;
     position: relative;
+    font-size: 18px;
 `;
 const LogoStart = styled.div`
     background: #ffa726;
@@ -35,10 +41,11 @@ const Menu = styled.div`
     margin-right: 20px;
 `;
 const Container = styled.div`
-    width: 1366px;
+    width: 100%;
+    /* width: 1366px; */
     display: flex;
     flex-direction: row;
-    justify-content: right;
+    justify-content: center;
     align-items: center;
 `;
 
